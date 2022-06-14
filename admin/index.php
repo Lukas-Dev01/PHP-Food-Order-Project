4b69af229fbee02b053b8c969eabe4c2628007cd
LukisXD <lukassakinis17@gmail.com>
2022-06-14 16:14:42 +0300

    “PHP-Food-Order-Project”

---
+++ admin/index.php
@@ -0,0 +1,29 @@
+<html>
+
+    <head>
+        <title>Food Order Website - Home Page</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
+    
+        <!-- Menu Section Starts -->
+        <div class="menu">
+            Menu Goes Here
+        </div>
+        <!-- Menu Section Ends -->
+
+        <!-- Main Content Section Starts -->
+        <div class="main-content">
+            Main Content Goes Here
+        </div>
+        <!-- Main Content Section Ends -->
+
+        <!-- Menu Section Starts -->
+        <div class="footer">
+            Footer Goes Here
+        </div>
+        <!-- Menu Section Ends  -->
+    
+    </body>
+</html>
